1edef347460e9c98f87dce80d0c37f060b8fe9b8
Quintus Labs <dev.quintus@gmail.com>
2021-03-03 16:42:12 +0530

    initial commit

--- set1.py
+++ set1.py
@@ -1,3 +1,4 @@
+# set union
 set1 = {"green", "blue"}
 set2 = {"blue", "yellow"}
 set3 = set1 | set2
---
+++ set2.py
@@ -0,0 +1,5 @@
+# set difference
+set1 = {"apple", "mango"}
+set2 = {"mango", "orange"}
+set3 = set1 & set2
+print(set3)
